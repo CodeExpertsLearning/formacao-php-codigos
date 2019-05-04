67ef44a778b6ffeca4d33284edeb7319c56f2a07
Nanderson Castro <nandokstro@gmail.com>
2019-05-04 12:13:04 -0300

    Dinamizando blog

---
+++ turma7/matutino/blog-php/index.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * Definindo constantes no PHP
+ */
+//Comentário de uma linha
+# Comentário de uma linha
+
+define('VIEWS_FOLDER', __DIR__ . '/views/');
+
+require VIEWS_FOLDER . 'index.phtml';
